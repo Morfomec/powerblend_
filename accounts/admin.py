d7d18591fd58a49bdf9e4a6ff067feb7cc5e066f
shifil <muhammedshifil@gmail.com>
2025-11-13 14:18:25 +0530

    CustomUser registered in default admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUser(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'is_active', 'is_staff', 'is_verified')
+
