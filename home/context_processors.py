5e768543134da6a4bfae384f54c24eaa54907a7c
Shifil <125294116+Morfomec@users.noreply.github.com>
2025-11-28 09:19:26 +0530

    Refactor wishlist_basket_item_counts for clarity

--- home/context_processors.py
+++ home/context_processors.py
@@ -16,30 +16,60 @@ def footer_product_links(request):
 
 
 
-def wishlist_basket_item_counts(request):
+# def wishlist_basket_item_counts(request):
 
-    """
-    to fetch the count of wishlist and basket items and return to show it on the icon
-    on header_layout.html
-    """
+#     """
+#     to fetch the count of wishlist and basket items and return to show it on the icon
+#     on header_layout.html
+#     """
    
+#     wishlist_count = 0
+#     basket_count = 0
+
+#     if request.user.is_authenticated:
+#         try:
+#             wishlist = request.user.wishlist
+#             wishlist_count = wishlist.items.count()
+#         except Exception:
+#             wishlist_count = 0
+
+#         try:
+#             basket = request.user.basket
+#             basket_count = sum(item.quantity for item in basket.items.all())
+#         except Exception:
+#             basket_count = 0
+
+#     # MUST return a dict
+#     return {
+#         'wishlist_count': wishlist_count,
+#         'basket_count': basket_count,
+#     }
+
+
+def wishlist_basket_item_counts(request):
+    """
+    Fetch count of wishlist and basket items and return for display on the header.
+    """
+    
     wishlist_count = 0
     basket_count = 0
 
-    if request.user.is_authenticated:
+    # Check safely if user exists and is authenticated
+    user = getattr(request, 'user', None)
+    
+    if user and user.is_authenticated:
         try:
-            wishlist = request.user.wishlist
+            wishlist = user.wishlist
             wishlist_count = wishlist.items.count()
         except Exception:
             wishlist_count = 0
 
         try:
-            basket = request.user.basket
+            basket = user.basket
             basket_count = sum(item.quantity for item in basket.items.all())
         except Exception:
             basket_count = 0
 
-    # MUST return a dict
     return {
         'wishlist_count': wishlist_count,
         'basket_count': basket_count,
